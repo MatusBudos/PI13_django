ed62a475c53370792b1b8a16456133dfe46752f2
Matúš Budoš <matusbudos21@gmail.com>
2023-12-01 11:38:37 +0100

    Vytvorenie view, pridanie triedy ako tabulky

--- skola/urls.py
+++ skola/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.vypis_students, name='vypis-stuents'),
+    path('', views.vypis_skola, name='vypis-skola'),
+    path('triedy/', views.vypis_triedy, name='vypis-tried'),
+    path('ucitelia/', views.vypis_ucitelov, name='vypis-ucitelov'),
+    path('studenti/', views.vypis_studentov, name='vypis-studentov'),
 ]
--- skola/views.py
+++ skola/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from . models import *
 
-def vypis_students(request):
-    studenti = Student.objects.all()
-    return(HttpResponse(studenti))
+def vypis_skola(request):
+    triedy = Trieda.objects.all().order_by("nazov")
+    ucitelia = Ucitel.objects.all().order_by("priezvisko")
+    studenti = Student.objects.all().order_by("priezvisko")
+    return render(request, "skola/index.html", {"triedy":triedy, "ucitelia":ucitelia, "studenti":studenti})
+    
+
+def vypis_studentov(request):
+    studenti = Student.objects.all().order_by("priezvisko")
+    return render(request, "skola/index.html", {"studenti":studenti})
+
+def vypis_triedy(request):
+    triedy = Trieda.objects.all().order_by("nazov")
+    return render(request, "skola/index.html", {"triedy":triedy})
+
+def vypis_ucitelov(request):
+    ucitelia = Ucitel.objects.all().order_by("priezvisko")
+    return render(request, "skola/index.html", {"ucitelia":ucitelia})
