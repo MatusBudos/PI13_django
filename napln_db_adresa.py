7bd7f83a59e7bac737aef0ed463c7f6cdbeb1a76
MatusBudos <91258409+MatusBudos@users.noreply.github.com>
2024-04-24 17:45:58 +0200

    hotovo - narodenie

--- napln_db_adresa.py
+++ napln_db_adresa.py
@@ -23,6 +23,16 @@ ucitelia = Ucitel.objects.all()
 for i in studenti:
     nahodny_riadok = random.choice(riadky)
     ulica, psc, obec = nahodny_riadok.split(";")
+    trieda = i.trieda.nazov[0]
+    if trieda == "1":
+        vek = 16
+    elif trieda == "2":
+        vek = 17
+    elif trieda == "3":
+        vek = 18
+    else:
+        vek = 19
+    i.vek = vek
     i.ulica = f"{ulica} {random.randint(1,1500)}" 
     i.psc = psc
     i.obec = obec
